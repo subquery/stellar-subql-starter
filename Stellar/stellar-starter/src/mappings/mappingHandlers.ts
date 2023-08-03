2a36e5599a74cb0de7317ec1a8f28b23ed713b5a
Naveen V <velnaveen99@gmail.com>
2023-08-03 11:35:55 +0000

    index payment operations

--- Stellar/stellar-starter/src/mappings/mappingHandlers.ts
+++ Stellar/stellar-starter/src/mappings/mappingHandlers.ts
@@ -1,50 +1,41 @@
-import { Ledger, Transaction, Operation, Effect } from "../types";
-import {SorobanBlock, SorobanTransaction, SorobanOperation, SorobanEffect} from "@subql/types-soroban";
-
-export async function handleBlock(block: SorobanBlock): Promise<void> {
-  logger.info(`Indexing ledger ${block.sequence}`);
-
-  const _block = Ledger.create({
-    id: block.id,
-    sequence: BigInt(block.sequence),
-    hash: block.hash
-  })
-
-  await _block.save();
-}
-
-export async function handleTransaction(tx: SorobanTransaction): Promise<void> {
-  logger.info(`Indexing transaction ${tx.hash}`)
-
-  const _tx = Transaction.create({
-    id: tx.id,
-    source_account: tx.source_account,
-    operation_count: tx.operation_count
-  })
-
-  await _tx.save();
-}
+import { Horizon } from "stellar-sdk";
+import { Payment, Credit, Debit } from "../types";
+import { SorobanOperation, SorobanEffect} from "@subql/types-soroban";
+import { AccountCredited, AccountDebited } from "stellar-sdk/lib/types/effects";
 
 export async function handleOperation(op: SorobanOperation): Promise<void> {
   logger.info(`Indexing operation ${op.id}, type: ${op.type}`)
 
-  const _op = Operation.create({
+  const _op = Payment.create({
     id: op.id,
-    account: op.source_account,
-    type: op.type,
-    txHash: op.transaction_hash
+    from: (op as unknown as Horizon.PaymentOperationResponse).from,
+    to: (op as unknown as Horizon.PaymentOperationResponse).to,
+    txHash: op.transaction_hash,
+    amount: (op as unknown as Horizon.PaymentOperationResponse).amount
   })
 
   await _op.save();
 }
 
-export async function handleEffect(effect: SorobanEffect): Promise<void> {
-  logger.info(`Indexing operation ${effect.id}, type: ${effect.type}`)
+export async function handleCredit(effect: SorobanEffect): Promise<void> {
+  logger.info(`Indexing effect ${effect.id}, type: ${effect.type}`)
 
-  const _effect = Effect.create({
+  const _effect = Credit.create({
     id: effect.id,
     account: effect.account,
-    type: effect.type
+    amount: (effect as AccountCredited).amount
+  })
+
+  await _effect.save();
+}
+
+export async function handleDebit(effect: SorobanEffect): Promise<void> {
+  logger.info(`Indexing effect ${effect.id}, type: ${effect.type}`)
+
+  const _effect = Debit.create({
+    id: effect.id,
+    account: effect.account,
+    amount: (effect as AccountDebited).amount
   })
 
   await _effect.save();
