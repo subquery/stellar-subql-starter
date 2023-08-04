c714500b4a9e7115ca86321afd66e754dffda188
Naveen V <velnaveen99@gmail.com>
2023-08-04 07:01:26 +0000

    use typing for operation and effect

--- Stellar/stellar-starter/src/mappings/mappingHandlers.ts
+++ Stellar/stellar-starter/src/mappings/mappingHandlers.ts
@@ -1,41 +1,41 @@
 import { Horizon } from "stellar-sdk";
 import { Payment, Credit, Debit } from "../types";
-import { SorobanOperation, SorobanEffect} from "@subql/types-soroban";
 import { AccountCredited, AccountDebited } from "stellar-sdk/lib/types/effects";
+import { SorobanOperation, SorobanEffect} from "@subql/types-soroban";
 
-export async function handleOperation(op: SorobanOperation): Promise<void> {
+export async function handleOperation(op: SorobanOperation<Horizon.PaymentOperationResponse>): Promise<void> {
   logger.info(`Indexing operation ${op.id}, type: ${op.type}`)
 
   const _op = Payment.create({
     id: op.id,
-    from: (op as unknown as Horizon.PaymentOperationResponse).from,
-    to: (op as unknown as Horizon.PaymentOperationResponse).to,
+    from: op.from,
+    to: op.to,
     txHash: op.transaction_hash,
-    amount: (op as unknown as Horizon.PaymentOperationResponse).amount
+    amount: op.amount
   })
 
   await _op.save();
 }
 
-export async function handleCredit(effect: SorobanEffect): Promise<void> {
+export async function handleCredit(effect: SorobanEffect<AccountCredited>): Promise<void> {
   logger.info(`Indexing effect ${effect.id}, type: ${effect.type}`)
 
   const _effect = Credit.create({
     id: effect.id,
     account: effect.account,
-    amount: (effect as AccountCredited).amount
+    amount: effect.amount
   })
 
   await _effect.save();
 }
 
-export async function handleDebit(effect: SorobanEffect): Promise<void> {
+export async function handleDebit(effect: SorobanEffect<AccountDebited>): Promise<void> {
   logger.info(`Indexing effect ${effect.id}, type: ${effect.type}`)
 
   const _effect = Debit.create({
     id: effect.id,
     account: effect.account,
-    amount: (effect as AccountDebited).amount
+    amount: effect.amount
   })
 
   await _effect.save();
