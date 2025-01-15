63830ef41703aa800d7d4d89356b7f4c09d8af58
Tate <yongzhao.tan@onfinality.io>
2025-01-15 03:47:44 +0000

    fix some bug

--- Stellar/soroban-greeter-contract/src/mappings/mappingHandlers.ts
+++ Stellar/soroban-greeter-contract/src/mappings/mappingHandlers.ts
@@ -2,11 +2,11 @@ import { Increment } from "../types";
 import { SorobanEvent } from "@subql/types-stellar";
 
 export async function handleEvent(event: SorobanEvent): Promise<void> {
-  logger.info(`Transaction hash: ${event.transaction.hash.toString()}`);
+  logger.info(`Transaction hash: ${event.transaction!.hash.toString()}`);
   if (event.type.toString() == "contract") {
     logger.info(`Event value: ${JSON.stringify(event.value)}`);
     const increment = Increment.create({
-      id: event.transaction.hash,
+      id: event.transaction!.hash,
       newValue: BigInt(
         JSON.parse(JSON.stringify(event.value))["_value"].toString(),
       ),
